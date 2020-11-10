01a475d7cac67052366df41ef6cbecd6feb4ec97
kmagdales <kyle.magdales@creditone.com>
2020-11-10 12:14:11 -0800

    create a test route

--- service/routes/routes.ts
+++ service/routes/routes.ts
@@ -1,7 +1,12 @@
 
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 export const apiRoutes = (router: Router) => {
 
+    router.get('/test', (_req: Request, res: Response) => {
+        res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+        <Response><Message>This is the Twilio + Credit One Bank Hackathon Winning Teams Test Response</Message></Response>`
+        )
+    } )
     return router;
 };
