92ac3ad5536ae3a374892cb8d8a7fd28b5b4e6cf
kmagdales <kyle.magdales@creditone.com>
2020-11-10 16:30:16 -0800

    added logic to store chat rooms in sql database and add redirect to request help response

--- service/controller/requestHelpController.ts
+++ service/controller/requestHelpController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 import { twilioClient } from '../services/twilioClient/twilioClient';
 import { PhoneNumberInstance } from 'twilio/lib/rest/lookups/v1/phoneNumber';
 import { constants } from '../constants/constants';
+import { v4 } from 'uuid';
+import { mySqlService } from '../services/mySql/mySqlService';
 
 export const requestHelpController = async (req: Request, res: Response) => {
     try {
@@ -10,14 +12,18 @@ export const requestHelpController = async (req: Request, res: Response) => {
         const twilioRes = await twilioClient.client.lookups.phoneNumbers(phone).fetch({type: ['carrier']});
 
         if (isMobile(twilioRes)) {
+            const roomId = await generateRoomId();
             twilioClient.client.messages.create({
-                body: 'Thank you for being a loyal customer. For help please use the following link to join a video chat with one of our reps. <link goes here>',
+                body: `Thank you for being a loyal customer. For help please use the following link to join a video chat with one of our reps. https://www.creditonebank.com?name=${roomId}`,
                 to: twilioRes.phoneNumber,
-                from: constants.TWILIO_NUMBER
+                from: constants.TWILIO_NUMBER,
+
             }).then(message => {
-                res.status(200).send(message);
+                res.status(200).send({
+                    result: `Message successfully sent to ${message.to}`
+                }).redirect(req.headers['referer']);
             }).catch(message => {
-                res.status(400).send(message);
+                res.status(400).send(message).redirect(req.headers['referer']);
             });
         } else {
             throw ({
@@ -27,10 +33,31 @@ export const requestHelpController = async (req: Request, res: Response) => {
         }
 
     } catch (error) {
-        res.status(error.status).send({message: error.message, code: error.code});
+        res.status(error.status).send({message: error.message, code: error.code}).redirect(req.headers['referer']);
     }
 };
 
 const isMobile = (phoneInstance: PhoneNumberInstance) => {
     return phoneInstance.carrier.type === 'mobile';
+}
+
+const generateRoomId = async (failureCount?: number) => {
+    const roomId = 'C1B_' + v4();
+
+    try {
+        await mySqlService.createRoom(roomId);
+        return roomId;
+    } catch (_err) {
+        if (!failureCount) {
+            failureCount = 1;
+        }
+        if (failureCount > 3) {
+            throw {
+                status: 400,
+                message: 'Unable to create video chat room'
+            }
+        } else {
+            await generateRoomId(failureCount + 1);
+        }
+    }
 }
--- service/services/mySql/mySqlService.ts
+++ service/services/mySql/mySqlService.ts
@@ -1,8 +1,94 @@
 
+import { getEnvVariable } from '../../env';
+import { Pool, createPool, PoolConfig, format } from 'mysql';
+
 class MySqlService {
+
+    private pool: Pool;
      
     public activate = () => {
+        const sessionOpts: PoolConfig = {
+            host: getEnvVariable('HACKATHON_DBHOST'),
+            user: getEnvVariable('HACKATHON_DBUSER'),
+            password: getEnvVariable('HACKATHON_DBPASS'),
+            database: getEnvVariable('HACKATHON_DB'),
+            dateStrings: true,
+            supportBigNumbers: true
+        };
+        this.pool = createPool(sessionOpts);
+    };
 
+    public createRoom = (roomId: string) => {
+        return new Promise<any>((resolve, reject) => {
+            const queryString = 'INSERT INTO CHAT_ROOMS (room_id, rep_joined, date_created) VALUES (?, ?, ?)';
+
+            const query = format(queryString, [roomId, 'false', new Date()]);
+
+            this.pool.query(query, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    };
+
+    public joinRoom = (roomId: string) => {
+        return new Promise<any>((resolve, reject) => {
+            const queryString = 'UPDATE CHAT_ROOMS SET rep_joined = ? WHERE room_id = ?';
+
+            const query = format(queryString, [roomId, 'true']);
+
+            this.pool.query(query, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    };
+
+    public removeRoom = (roomId: string) => {
+        return new Promise<any>((resolve, reject) => {
+            const queryString = 'DELETE FROM CHAT_ROOMS WHERE room_id = ?';
+
+            const query = format(queryString, [roomId]);
+
+            this.pool.query(query, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    };
+
+    public selectAllRooms = () => {
+        return new Promise<any>((resolve, reject) => {
+            const query = 'SELECT * FROM CHAT_ROOMS';
+
+            this.pool.query(query, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    };
+
+    public selectAllUnjoinedRooms = () => {
+        return new Promise<any>((resolve, reject) => {
+            const queryString = 'SELECT * FROM CHAT_ROOMS WHERE rep_joined = ?';
+            
+            const query = format(queryString, ['false']);
+
+            this.pool.query(query, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
     };
 }
 
